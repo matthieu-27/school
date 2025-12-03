d45aa006414bca4f0bafff06c7db77b9abda3138
matthieu-27 <matthieu27@pm.me>
2025-12-03 14:24:56 +0100

    feat: add real_all courses function

--- school.py
+++ school.py
@@ -8,10 +8,6 @@ if __name__ == "__main__":
 
     school: School = School()
 
-    # initialisation d'un ensemble de cours, enseignants et élèves composant l'école
-    school.init_static()
-
     # affichage de la liste des cours, leur enseignant et leurs élèves
-    school.display_courses_list()
-
-    print(school.get_course_by_id(1))
+    for course in school.display_courses_list(school):
+        print(course)
